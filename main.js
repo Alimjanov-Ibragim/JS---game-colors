6b3c01c9726605a438bf5897e7d645b81608a202
Alimjanov-Ibragim <ballismlife@gmail.com>
2019-06-27 12:32:24 +0500

    Объект события

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 var divs = document.querySelectorAll('div')
+var link = document.querySelector('a')
 
 for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', function(){
+    divs[i].addEventListener('click', function(event){
+        event.stopPropagation()
         console.log(this.getAttribute('id'))
-    }, true)
+    })
+}
+
+link.addEventListener('click', handleLinkClick)
+
+function handleLinkClick(event) {
+    event.preventDefault()
+
+    var div = divs[0]
+
+    // if (div.style.display === 'none') {
+    //     div.style.display = 'flex'
+    // } else {
+    //     div.style.display = 'none'
+    // }
+
+    div.style.display = div.style.display === 'none'
+        ? 'flex'
+        : 'none'
+
+    console.log(div.style.display)
 }
